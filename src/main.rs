0498c7785f94d4b45cd8e73ad798a3a77dcf0d1f
Idemar <mortenidemar@gmail.com>
2024-12-16 21:24:01 +0100

    test

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,22 @@
-mod kamera;
-mod romskip;
 mod bevegelse;
 mod debug;
+mod kamera;
+mod romskip;
 
 use bevy::prelude::*;
-use bevy::prelude::Color;
-use kamera::KameraPlugin;
-use debug::DebugPlugin;
+
 use bevegelse::BevegelsePlugin;
+use debug::DebugPlugin;
+use kamera::KameraPlugin;
 use romskip::RomskipPlugin;
 
 fn main() {
     App::new()
-        .insert_resource(ClearColor(Color::rgb(0.1, 0.0, 0.15)))
-        .insert_resource(AmbientLight {color: Color::default(), brightness: 0.75})
+        .insert_resource(ClearColor(Color::srgb(0.1, 0.0, 0.25)))
+        .insert_resource(AmbientLight {
+            color: Color::default(),
+            brightness: 1200.0,
+        })
         .add_plugins(DefaultPlugins)
         .add_plugins(BevegelsePlugin)
         .add_plugins(DebugPlugin)
